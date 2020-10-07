485caf4129a1e33a22509ad53f97f6106a223917
Borja <Borja@LAPTOP-UE311JRI>
2020-10-07 11:31:19 +0100

    First project

---
+++ src/kata1/Person.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata1;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Borja
+ */
+public class Person {
+    private final String name;
+    private final LocalDate birthname;
+    public Person(String name,LocalDate birthname) {
+        this.name=name;
+        this.birthname=birthname;
+    }
+
+    Person() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthname() {
+        return birthname;
+    }
+    
+    public int getAge(){
+        return toYears(LocalDate.now().toEpochDay() - birthname.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days/365);
+    }
+    
+    
+    
+    
+    
+    
+}
